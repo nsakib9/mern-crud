2dfee7fe41bd508ce0ed56fefc0a4dd2a6e5663c
nsakib9 <sakibkhan207@gmail.com>
2022-01-04 07:03:52 +0600

    2**Add 4 files, Update PostList, PostItem

---
+++ client/src/PostItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PostItem() {
+    return(
+        <div>
+            <h1>
+                This is our list component
+            </h1>
+        </div>
+    )
+}
+
+export default PostItem;
---
+++ client/src/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PostList() {
+    return(
+        <div>
+            <h1>
+                This is our list component
+            </h1>
+        </div>
+    )
+}
+
+export default PostList
