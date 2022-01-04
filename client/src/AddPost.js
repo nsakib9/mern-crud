914f1152d65212a5ff5234505d911d993afd0144
nsakib9 <sakibkhan207@gmail.com>
2022-01-04 07:09:35 +0600

    4-Update Addpost, Editpost

---
+++ client/src/AddPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AddPost() {
+    return(
+        <div>
+            <h1>
+                This is our AddPost component
+            </h1>
+            <PostItem />
+        </div>
+    )
+}
+
+export default AddPost;
---
+++ client/src/EditPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function EditPost() {
+    return(
+        <div>
+            <h1>
+                This is our Edit Post component
+            </h1>
+            <PostItem />
+        </div>
+    )
+}
+
+export default EditPost;
