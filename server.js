0a1ba4cd71a74bfc58e69b457ffe863bfb67bbe1
nsakib9 <sakibkhan207@gmail.com>
2022-01-05 23:40:59 +0600

    9-set / route with message check nodemon server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.listen(5000, function(){
-    console.log('Node Js and Express Server Started Successfully')
+app.get('/', (req, res)=>{
+    res.end('Hello World with node js')
 })
+
+app.listen(5000, function(){
+    console.log('Node Js and Express Server Started Successfully auto start')
+})
+
+
