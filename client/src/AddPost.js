aa13b375249f7d8e0fe112e59c82de4dfd6fffd8
nsakib9 <sakibkhan207@gmail.com>
2022-01-06 07:36:04 +0600

    15-imp hooks for form field and check with console

--- client/src/AddPost.js
+++ client/src/AddPost.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddPost() {
+    const [title, settitle] = useState('')
+    const [imageurl, setimageurl] = useState('')
+    const [description, setdescription] = useState('')
+
+    function addpost() {
+        var post = {
+            title: title,
+            imageurl: imageurl,
+            description: description
+        }
+        console.log(post);
+    }
+
     return(
         <div className="row justify-content-center">
             <div className="col-md-6">
                 <div>
-                    <input type="text" placeholder='title' className="form-control" />
-                    <input type="text" placeholder='imageUrl' className="form-control" />
-                    <textarea cols='30' rows="10" placeholder="Description" className="form-control" />
+                    <input type="text" placeholder='title' className="form-control" value={title} onChange={(e)=> {settitle(e.target.value)}} />
+                    <input type="text" placeholder='imageUrl' className="form-control" value={imageurl} onChange={(e)=>{setimageurl(e.target.value)}} />
+                    <textarea cols='30' rows="10" placeholder="Description" className="form-control" value={description} onChange={(e)=>{setdescription(e.target.value)}} />
                     
 
-                    <button className="btn btn-success">Add Post</button>
+                    <button onClick={addpost} className="btn btn-success">Add Post</button>
                 </div>
             </div>
         </div>
